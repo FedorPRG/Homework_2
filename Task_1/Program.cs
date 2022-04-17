67c939a50b3d97f05b74bd4646daa1254679efb1
Fedor <fedor_prg@rambler.ru>
2022-04-17 15:49:24 +1000

    Created Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int decision(int a)
+{
+    int result = a / 100 * 10 + a % 10;
+    return result;
+}
+
+int a = new Random().Next(100,1000);
+Console.WriteLine($"{a}->{decision(a)}");
+
+a = new Random().Next(100,1000);
+Console.WriteLine($"{a}->{decision(a)}");
+
+a = new Random().Next(100,1000);
+Console.WriteLine($"{a}->{decision(a)}");
