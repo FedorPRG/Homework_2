1546c1f671d69f6b9b586e2d3ade3b411300d403
Fedor <fedor_prg@rambler.ru>
2022-04-17 16:49:37 +1000

    Created Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите цифру: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+int index = 1;
+int discharge = 10;
+while (index < 1000)
+{
+    if (number / discharge == 0)
+    {
+        break;
+    }
+    discharge = discharge * 10; index = index + 1;
+}
+Console.WriteLine(discharge);
+Console.WriteLine(index);
+Console.WriteLine(number);
