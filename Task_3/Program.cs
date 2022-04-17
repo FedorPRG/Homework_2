74ae3ab02e88fa5a5129733d5405c7e974fc4286
Fedor <fedor_prg@rambler.ru>
2022-04-17 15:53:03 +1000

    Created Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 | number == 7)
+{
+    Console.WriteLine($"{number}-> День выходной");
+}
+else Console.WriteLine($"{number}-> День не выходной");
+
